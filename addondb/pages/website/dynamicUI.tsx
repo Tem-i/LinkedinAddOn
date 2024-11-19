c1baf3f61dcbc7fae9aed2ded42380658e5bc0de
Tem-i <petert9@rpi.edu>
2024-11-19 17:04:40 -0500

    new commit

---
+++ addondb/pages/website/dynamicUI.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            <div className = 'w-full h-screen bg-pink-300'> </div>
+
+        </div>
+    )
+}
+
+export default Home;
+//to start the next js project, type "npm run dev" command into the terminal
